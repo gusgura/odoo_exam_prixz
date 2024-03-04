260c485902f0865a226bb7ce346d98d97db7b737
gusgura <60492044+gusgura@users.noreply.github.com>
2024-03-04 15:14:45 -0600

    Add files via upload

--- __manifest__.py
+++ __manifest__.py
@@ -20,13 +20,14 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale'],
+    'depends': ['base', 'sale', 'stock'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/stock_picking_view.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
--- models/stock_picking.py
+++ models/stock_picking.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
+import requests
+import json
 from odoo import models, fields, api
-
 
 class stockPicking(models.Model):
     _inherit = 'stock.picking'
 
     reservado_total = fields.Float(compute='get_reservado')
 
-    #@api.onchange('partner_id')
     def get_reservado(self):
         print("reservado")
         for record in self:
@@ -19,15 +19,21 @@ class stockPicking(models.Model):
             if reservado > 0:
                 record.reservado_total = reservado
                 print("reservado: ",record.reservado_total)
-    # _name = 'odoo_exam_prixz.odoo_exam_prixz'
-#     _description = 'odoo_exam_prixz.odoo_exam_prixz'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    def get_products_api(self):
+        url = "https://fakestoreapi.com/products"
+        # api_key = "a57b8608-76ec-4ec6-8459-fd2e8d962db2"
+        # if not self.env['ir.config_parameter'].sudo().get_param('database.is_neutralized', False):
+        #     url = self.env['ir.config_parameter'].sudo().get_param('VTECH_Asismed_Medikit.medikit_url', False)
+        #     api_key = self.env['ir.config_parameter'].sudo().get_param('VTECH_Asismed_Medikit.medikit_api_key', False)
+        # payload = "{\r\n    \"NUR\":\"%s\"\r\n}\r\n"%(nur)
+        # headers = {
+        #     'API-KEY': api_key
+        # }
+        response = requests.get(url)#, headers=headers, data=payload)
+        content_decode = response.content.decode("utf-8")
+        content_json = json.loads(content_decode)
+        print("API: ",content_json)
+        # if content_json['Error'] != 'OK':
+        #     raise UserError(content_json['Error'])
+        # return content_json
